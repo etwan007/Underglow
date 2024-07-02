a54ce6980161d385079b77ad2d2b9afbf0db9551
Ethan Fuller <etwan007@gmail.com>
2024-07-02 09:36:33 -0500

    Doesnt fully work but semi close

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,18 @@
 #include <FastLED.h>
 #include <LittleFS.h>
 #include <ESP8266WebServer.h>
-#include "animations.h" // Include the animations header
 
 #define LED_PIN D1 // GPIO5 (D1 on most ESP8266 boards)
-const int NUM_LEDS = 200; // Update this to the number of LEDs in your strip
-
+#define SWITCH D2
+const int NUM_LEDS = 10; // Update this to the number of LEDs in your strip
 
 CRGB leds[NUM_LEDS];
 ESP8266WebServer server(80);
-int i = 0;
 
 bool colorChanged = false;
 bool animationChanged = false;
-byte newR, newG, newB;
-int newAnimationIndex = 0;
+byte newR = 255, newG = 0, newB = 0; // Default to red
+int newAnimationIndex = -1;
 
 // Set your static IP address
 IPAddress local_IP(192, 168, 4, 1);
@@ -27,13 +25,20 @@ void handleRoot();
 void handleColor();
 void handleAnimationRequest();
 void setColor(byte r, byte g, byte b);
+void displayAnimation(int index);
+void rainbow_wave();
+void confetti();
+void sinelon();
+void juggle();
+void bpm();
+
+bool isAnimationActive = false; // To track if an animation is active
+bool colorSet = false; // To track if the color has been set
 
 void setup() {
   Serial.begin(115200);
-  FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2811, LED_PIN, BRG>(leds, NUM_LEDS);
   FastLED.setBrightness(99);
-  fill_solid(leds, NUM_LEDS, CRGB::Red); // Initialize all pixels to 'off'
-  FastLED.show();
 
   // Set up the ESP8266 as an access point with a static IP
   WiFi.softAPConfig(local_IP, gateway, subnet);
@@ -53,33 +58,45 @@ void setup() {
   server.serveStatic("/style.css", LittleFS, "/style.css");
   server.serveStatic("/iro.min.js", LittleFS, "/iro.min.js");
 
-  server.on("/color", HTTP_GET, handleColor);
+  server.on("/color", HTTP_POST, handleColor);
   server.on("/animation", HTTP_GET, handleAnimationRequest);
 
   server.begin();
   Serial.println("HTTP server started");
+
+  pinMode(SWITCH, OUTPUT);
+  digitalWrite(SWITCH, HIGH);
+  setColor(newR, newG, newB); // Initialize with the default color
 }
 
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = {rainbow_wave, confetti, sinelon, juggle, bpm};
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
 void loop() {
- server.handleClient();
+  server.handleClient();
 
-  if (colorChanged) {
-    setColor(newR, newG, newB);
-    colorChanged = false; // Reset the flag after updating
-    FastLED.show();
-    Serial.println("Color updated");
-    delay(800);
+  if (digitalRead(SWITCH) == HIGH) {
+    if (isAnimationActive) {
+      isAnimationActive = false;
+      fill_solid(leds, NUM_LEDS, CRGB(newR, newG, newB));
+      FastLED.show();
+      Serial.println("Switching to solid color");
+    }
+  } 
+  else {
+    if (!isAnimationActive) {
+      isAnimationActive = true;
+      Serial.println("Switching to animation");
+    }
+    displayAnimation(newAnimationIndex >= 0 ? newAnimationIndex : 0);
+    FastLED.show(); // Call FastLED.show() only in animation mode
+    delay(10);
   }
 
-  if (animationChanged) {
-    handleAnimation(newAnimationIndex);
-    animationChanged = false; // Reset the flag after updating
-    Serial.println("Animation updated");
-  }
-
-  fill_solid(leds, NUM_LEDS, CRGB(190, 0, 255));
-  FastLED.show();
-  
+  delay(100);
 }
 
 void handleRoot() {
@@ -93,27 +110,78 @@ void handleRoot() {
 }
 
 void handleColor() {
-  String color = server.arg("value"); // Changed from 'c' to 'value' to match the client-side code
-  long number = strtol(color.c_str(), NULL, 16); // No need to remove '#'
-  newR = (number >> 16) & 255;
-  newG = (number >> 8) & 255;
-  newB = number & 255;
-  colorChanged = true; // Set the flag to indicate the color needs updating
-  server.send(200, "text/plain", "Color set");
-  Serial.printf("Color set to: R=%d, G=%d, B=%d\n", newR, newG, newB);
+  if (server.hasArg("value")) { // Check if the 'value' argument is present
+    String color = server.arg("value");
+    Serial.print(color);
+    long number = strtol(color.c_str(), NULL, 16);
+    newR = (number >> 16) & 255;
+    newG = (number >> 8) & 255;
+    newB = number & 255;
+
+    // Set the color and reset animation index
+    fill_solid(leds, NUM_LEDS, CRGB(newR, newG, newB));
+    FastLED.show();
+    newAnimationIndex = -1;
+    isAnimationActive = false;
+    colorSet = true; // Mark that the color has been set
+    Serial.printf("Color set to: R=%d, G=%d, B=%d\n", newR, newG, newB);
+  }
 }
 
 void handleAnimationRequest() {
   String anim = server.arg("a");
   newAnimationIndex = anim.toInt();
   animationChanged = true; // Set the flag to indicate the animation needs updating
-  server.send(200, "text/plain", "Animation started");
+  server.send(200, "text/plain", "Animation set");
   Serial.printf("Animation set to: %d\n", newAnimationIndex);
 }
 
 void setColor(byte r, byte g, byte b) {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(r, g, b);
+  FastLED.clear();
+  fill_solid(leds, NUM_LEDS, CRGB(r, g, b));
+  colorSet = true; // Ensure the color has been set
+  delay(10);
+  
+  FastLED.show();
+}
+
+void displayAnimation(int index) {
+  if (index >= 0 && index < 5) {
+    gPatterns[index]();
   }
 }
 
+void rainbow_wave() {
+  uint8_t thisHue = beat8(10, 255);  // A simple rainbow march.
+  fill_rainbow(leds, NUM_LEDS, thisHue, 10);  // Use FastLED's fill_rainbow routine.
+}
+
+void confetti() {
+  fadeToBlackBy(leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV(gHue + random8(64), 200, 255);
+}
+
+void sinelon() {
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+  int pos = beatsin16(13, 0, NUM_LEDS - 1);
+  leds[pos] += CHSV(gHue, 255, 192);
+}
+
+void bpm() {
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8(BeatsPerMinute, 64, 255);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = ColorFromPalette(palette, gHue + (i * 2), beat - gHue + (i * 10));
+  }
+}
+
+void juggle() {
+  fadeToBlackBy(leds, NUM_LEDS, 20);
+  uint8_t dothue = 0;
+  for (int i = 0; i < 8; i++) {
+    leds[beatsin16(i + 7, 0, NUM_LEDS - 1)] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
